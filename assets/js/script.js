8a33410fb871e614b7b11d8d80eb17fa6443c83a
William Brown <willis291@gmail.com>
2023-05-31 18:56:02 -0600

    improved layout. Added OpenWeather functionality. Added text input field for specifying location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var requestURL =
-"https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=03367a7d999670d5ffb1a5e0906afaaa";
+  "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=03367a7d999670d5ffb1a5e0906afaaa";
+
+var currentWeatherText = $("#current-weather");
+var currentWeatherImg = $("#current-weather-image");
+var imageSourceBaseURL = "https://openweathermap.org/img/wn/";
 
 function weatherFetcher() {
   console.log("function started");
@@ -8,11 +12,19 @@ function weatherFetcher() {
       return response.json();
     })
     .then(function (data) {
-      weatherObject = data;
-      console.log("URL of dog photo is: " + weatherObject); //Will display the URL of the dog photo in the console
+      var weatherObject = data;
+      var conditions = weatherObject.weather[0];
+      console.log(weatherObject); //Will display the URL of the dog photo in the console
+      //   console.log(data.weather[0].main);
+      currentWeatherText.text(conditions.main);
+      currentWeatherImg.attr(
+        "src",
+        imageSourceBaseURL + conditions.icon + "@4x.png"
+      );
+      $("#current-temp").text(weatherObject.main.temp);
+      $("#current-wind").text(weatherObject.wind.speed);
+      $("#current-humidity").text(weatherObject.main.humidity);
     });
-}
-
 }
 
 weatherFetcher();
