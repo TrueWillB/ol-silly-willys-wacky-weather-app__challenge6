9339e87bac924c60c9b1b90e240662354194cad4
William Brown <willis291@gmail.com>
2023-06-01 10:44:48 -0600

    Added the fetch function for coordinates. Need to get the data out of the function, need to learn about and implement asynchronous variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var testRequestURL =
-  "https://api.openweathermap.org/data/2.5/forecast/daily?lat=51.5073219&lon=-0.1276474&cnt=3&appid=03367a7d999670d5ffb1a5e0906afaaa";
+  "http://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=5&appid=03367a7d999670d5ffb1a5e0906afaaa";
+//   "https://api.openweathermap.org/data/2.5/forecast/daily?lat=51.5073219&lon=-0.1276474&cnt=3&appid=03367a7d999670d5ffb1a5e0906afaaa";
 
 var requestURL =
   "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=03367a7d999670d5ffb1a5e0906afaaa";
 var baseRequestURL = "https://api.openweathermap.org/data/2.5/weather?";
+var baseGeoRequestURL = "http://api.openweathermap.org/geo/1.0/direct?q=";
 var URLEnding = "&units=imperial&appid=03367a7d999670d5ffb1a5e0906afaaa";
 
 var currentWeatherText = $("#current-weather");
@@ -11,11 +13,26 @@ var currentWeatherImg = $("#current-weather-image");
 var imageSourceBaseURL = "https://openweathermap.org/img/wn/";
 // var targetCity = "Denver";
 
+//TODO figure out async data
 //TODO Add function that stores all of the previously entered cities, in an array and in permanent storage. The array should then build the history up until 10 items.
 //TODO figure out wtf you're doing for the forecast, it is very unclear
 //TODO create geolocation function for entered city, needed for the 5 day forecast
 function weatherFetcher(location) {
-  console.log("function started");
+  //   var coordinates = fetchCoordinates(location);
+
+  var geoRequestURL =
+    baseGeoRequestURL +
+    location +
+    "&limit=5&appid=03367a7d999670d5ffb1a5e0906afaaa";
+  console.log("The URL is: " + geoRequestURL);
+  fetch(geoRequestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      geocodeObject = data;
+      var coordinates = [geocodeObject[0].lat, geocodeObject[0].lon];
+    });
 
   requestURL = baseRequestURL + "q=" + location + URLEnding;
 
@@ -26,7 +43,7 @@ function weatherFetcher(location) {
     .then(function (data) {
       var weatherObject = data;
       var conditions = weatherObject.weather[0];
-      console.log(weatherObject); //Will display the URL of the dog photo in the console
+      //   console.log(weatherObject);
       //   console.log(data.weather[0].main);
       $("#current-weather-location-text").text(weatherObject.name);
       currentWeatherText.text(conditions.main);
@@ -62,3 +79,20 @@ function testFetcher() {
       console.log(data);
     });
 }
+
+// function fetchCoordinates(location) {
+//   var geoRequestURL =
+//     baseGeoRequestURL +
+//     location +
+//     "&limit=5&appid=03367a7d999670d5ffb1a5e0906afaaa";
+//   console.log("The URL is: " + geoRequestURL);
+//   fetch(geoRequestURL)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       geocodeObject = data;
+//       var coordinates = [geocodeObject[0].lat, geocodeObject[0].lon];
+//       //   console.log(coordinates);
+//     });
+// }
